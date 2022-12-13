f13778b458ea64afd752c9b10fc97018e65345e1
Prizzz <rob_243248@mail.ru>
2022-12-14 02:18:03 +0400

    feat: add feed and masonry layout

---
+++ shareme_frontend/src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TailSpin } from 'react-loader-spinner';
+
+const Spinner = ({ message }) => {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <TailSpin color="#ef4444" height={100} width={100} className="m-5" />
+      <p className="text-lg text-center px-2">{message}</p>
+    </div>
+  );
+};
+
+export default Spinner;

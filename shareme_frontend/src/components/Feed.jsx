0d890efa5e0f25b1c698e9951f8c77155e82e70b
Prizzz <rob_243248@mail.ru>
2022-12-17 21:36:06 +0400

    feat: add createPin

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -22,7 +22,6 @@ const Feed = () => {
         setLoading(false);
       });
     } else {
-      console.log(1);
       client.fetch(feedQuery).then((data) => {
         setPins(data);
         setLoading(false);
